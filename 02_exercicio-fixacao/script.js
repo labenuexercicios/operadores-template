0c4db720f2e56df0ef478a3e9a651cf244886770
Tayanne <tayh.snovaes@gmail.com>
2022-07-20 17:32:31 -0300

    [fix]

---
+++ 02_exercicio-fixacao/script.js
@@ -0,0 +1,9 @@
+// EXERCICIO 01
+const minhaIdade = prompt('Qual sua idade?')
+const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+
+// EXERCICIO 02
+const a = 'vou pra praia'
+const b = 'sou maior de idade'
+const c = 'posso tomar uma cervejinha'
+const d = 'quero passear de carro'
