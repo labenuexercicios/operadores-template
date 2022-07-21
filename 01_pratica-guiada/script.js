87e7b6ffb8c88798f2717c71ca32afd97da44247
JulianDiel <drdiel@hotmail.com>
2022-07-20 21:30:44 -0300

    exercicios em aula

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,70 @@
 //CODIGO AQUI
+// let saldo;
+// saldo = 15;
+// saldo = saldo + 22.40;
+// console.log(`Soma `, saldo);
+
+// saldo-= 12.34;
+// console.log(`Subtração `, saldo);
+
+// saldo/= 3;
+// console.log(`Divisão `, saldo)
+
+// saldo *= 3.42
+// console.log(`Multiplicação `, saldo)
+
+// saldo %=3
+// console.log(`resto `, saldo)
+
+
+// const multiESoma1 = 2*2+2;
+// console.log(`MS1`,multiESoma1);
+
+// const multiESoma2 = 2*(2+2);
+// console.log(`MS1`,multiESoma2);
+
+// const soma1 = 2*2+2;
+// console.log(`soma1`, soma1);
+
+// const soma2 = 2*(2+2);
+// console.log(`soma2`, soma2);
+
+
+// //IGUAL OU DIFERENTE
+// const resultado1 = 5===5;
+// console.log(`r1`, resultado1); 
+// const resultado2 = 5!==5;
+// console.log(`r2`, resultado2); 
+// const resultado3 =  5!=='5'
+// console.log(`r3`, resultado3);
+// const resultado4 = '5' === 'cinco'
+// console.log(`r4`, resultado4);
+// const resultado5 = typeof 5 === typeof 20
+// console.log(`r5`, resultado5);
+// const resultado6 = typeof '5' === typeof 'cinco'
+// console.log(`r6`, resultado6);
+
+
+//MAIOR IGUAL - MENOR IGUAL
+
+// let primeiroN = 15
+// let segundoN = 12
+// console.log(`O primeiro numero é igual ao segundo?`, primeiroN === segundoN);
+// console.log(`O primeiro numero é menor igual ao segundo?`, primeiroN <= segundoN);
+// console.log(`O primeiro numero é maior que o segundo?`, primeiroN > segundoN);
+// console.log(`O primeiro numero é menor que o segundo?`, primeiroN < segundoN);
+
+
+//LÓGICA && || !
+
+// const a = 'Quero dirigir um Celta 2012';
+// const b = 'Sou maior de idade';
+// const c = 'Consigo comprar o celta por 22.000 reais';
+// const d = 'Consigo Alugar um Celta';
+// const e = 'Tenho carteira de motorista';
+
+// a
+// !b
+// b && e
+// b && (c || d)
+// (b && e) && !a
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,9 +1,17 @@
 // EXERCICIO 01
-const minhaIdade = prompt('Qual sua idade?')
-const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+const minhaIdade = Number(prompt('Qual sua idade?'))
+const idadeAmiga = Number(prompt('Qual a idade da sua amiga ou amigo?'))
+const diferencaIdade = minhaIdade - idadeAmiga
+console.log(`Sua idade é maior do que a do seu melhor amigo? `, minhaIdade > idadeAmiga, `A diferença de idade entre vocês é `, diferencaIdade, `ano(s)`);
+
 
 // EXERCICIO 02
 const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+!b && d
+(b && a) && !d
+a || d
+c && !d
