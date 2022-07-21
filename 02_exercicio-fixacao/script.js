c909865bbcec2292f9d938a09c0f4e3516319435
Nayara <nayaraazevwdo@gmail.com>
2022-07-20 21:17:02 -0300

    01_pratica_guiada

--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,9 +1,19 @@
 // EXERCICIO 01
-const minhaIdade = prompt('Qual sua idade?')
-const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+// const minhaIdade = prompt('Qual sua idade?')
+// const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+// console.log( minhaIdade > idadeAmiga)
+// console.log( Number(minhaIdade) - Number(idadeAmiga))
 
 // EXERCICIO 02
-const a = 'vou pra praia'
-const b = 'sou maior de idade'
-const c = 'posso tomar uma cervejinha'
-const d = 'quero passear de carro'
+
+// const a = 'vou pra praia'
+// const b = 'sou maior de idade'
+// const c = 'posso tomar uma cervejinha'
+// const d = 'quero passear de carro'
+
+//!b && d //E
+//(b && a) && !d
+//a || d //OU
+//c && !d
+
+
