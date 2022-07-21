08f6ac752fc6a3a4cfb3cc9c22802d5b03e35d26
anapgouveia <anapgouveia97@gmail.com>
2022-07-20 21:31:45 -0300

    aula pratica-variaveis

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,99 @@
 //CODIGO AQUI
+
+let saldo = 15 + 22.40
+console.log("saldo inicial", saldo)
+
+saldo = saldo - 12.34
+console.log("saldo subtraido", saldo)
+
+saldo = saldo / 3
+console.log("saldo dividido", saldo)
+
+saldo = saldo * 3.42
+console.log("saldo aplicado", saldo)
+
+saldo = saldo % 3
+console.log("resto", saldo)
+
+//ordem
+const multESoma = 2*2+2
+console.log(multESoma)
+
+const somaEMult = 2*(2+2)
+console.log(somaEMult)
+
+//IGUAL E DIFERENTE
+// comparaçao de valores == (igualdade)
+//comparaçao de valor e tipo === (igualdade)
+// comparaçao de valor != (difença)
+//comparaçao de valor e tipo !== (diferença)
+
+
+//comparaçao vai retornar true pq sao do mesmo tipo
+let comparacaoIgual = 5 === 5
+console.log("comparacaoIgual", comparacaoIgual)
+
+//comparaçao vai retornar false pq sao do mesmo tipo
+let comparacaoDiferente = 5 !==5
+console.log("comparacao diferente", comparacaoDiferente)
+
+//comparaçao number com uma string (number !== string), por isso é true pq o number é diferente de string
+let comparacaoTipo1 = 5 !== "5"
+console.log("comparacaoTipo1", comparacaoTipo1)
+
+// != ele ver o valor e o !== ver o valor e o tipo
+
+// sao duas strings porem o seu valor nao é igual
+let comparacaoTipo2 = "5" === "cinco"
+console.log("comparacaoTipo2",
+comparacaoTipo2)
+
+// comparando number com number entao vai ser true. comparando valor e tipo
+let comparacaoTipo3 = typeof 5 === typeof 5
+console.log("comparacaoTipo3", comparacaoTipo3)
+
+// lendo number === number
+let comparacaoTipo4 = typeof 5 === typeof 20
+console.log("comparacaoTipo4", comparacaoTipo4)
+
+//MENOR E MAIOR (OU IGUAL)
+//criando e declarando variavel: 
+const num1 = 2
+const num2 = 8
+let resultado
+
+//1- O primeiro número é igual ao segundo? 
+resultado = num1 === num2
+console.log("o primeiro numero é igual aos egundo?", resultado )
+
+//2- O primeiro número é menor ou igual ao segundo? 
+resultado = num1 <= num2
+console.log("O primeiro número é menor ou igual ao segundo?", resultado)
+
+//3- O primeiro número é maior que o segundo? T
+resultado = num1 > num2
+console.log("O primeiro número é maior que o segundo?", resultado)
+
+//4- O primeiro número é menor que o segundo? 
+resultado = num1 < num2
+console.log("O primeiro número é menor que o segundo?", resultado)
+
+//OPERADORES LOGICOS- condições "se, senao"
+//NÃO
+
+let a = "Quero dirigir um Celta 2012"
+let b = "Sou maior de idade"
+let c = "consigo comprar o Celta por 22.000 reais"
+let d = "consigo alugar o Celta"
+let e = "tenho carteira de motorista"
+
+a
+!b
+b && !e
+b && c || d
+b && e && !a
+
+
+
+
+
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -2,8 +2,24 @@
 const minhaIdade = prompt('Qual sua idade?')
 const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
 
+let comparacaoIdade = minhaIdade > idadeAmiga
+console.log("minha idade é maior que o da minha amiga?", comparacaoIdade)
+
+let difIdade = minhaIdade - idadeAmiga
+console.log("Diferença de idade", difIdade)
+
+
+
+
+
 // EXERCICIO 02
 const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+!b && d 
+b && a && !d
+a || d
+c && !d
+
