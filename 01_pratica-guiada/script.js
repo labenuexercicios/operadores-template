f607df69e0a5e7fa2f6f5198db1a193b334b667a
MilenaCruzJusta <milenajusta86@gmail.com>
2022-07-21 05:51:33 -0300

    subindo a008

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,53 @@
 //CODIGO AQUI
+let saldo = 15 + 22.40
+console.log("saldo inicial", saldo)
+
+saldo = saldo - 12.34
+console.log("saldo subtraído", saldo)
+
+let divisao1 = saldo / 3
+console.log(divisao1)
+
+let aplicaçao = divisao1 * 3.42
+console.log(aplicaçao)
+
+let chiclete = aplicaçao % 3
+console.log(chiclete)
+
+let teste1 = 2 * 2 + 2
+console.log(teste1)
+
+let teste2 = 2 * (2+2)
+console.log(teste2)
+
+comparaçao1 = 5 === 5
+console.log(comparaçao1)
+
+comparaçao2 = 5!== 5
+console.log(comparaçao2)
+
+comparaçao3 = 5 !== "5"
+console.log(comparaçao3)
+
+comparaçao4 = "5" === "cinco"
+console.log(comparaçao4)
+
+comparaçao5 = typeof 5 === typeof 20
+console.log(comparaçao5)
+
+comparaçao6 = typeof "5" === typeof "cinco"
+console.log(comparaçao6)
+
+A = 11
+B = 22
+
+console.log(A >= B)
+console.log(A <= B)
+console.log(A > B)
+console.log(A < B)
+
+a
+! b
+b && ! e
+b && (c || d)
+(b && e) && ! a
