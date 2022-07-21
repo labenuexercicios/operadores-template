c98320e0689a9a0cc3afff0d1514e4b096b32d71
Raul Cesar <raulcesar.rcf@gmail.com>
2022-07-20 21:11:04 -0300

    Atividade de JS, utilizando operadores

--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,9 +1,24 @@
 // EXERCICIO 01
 const minhaIdade = prompt('Qual sua idade?')
 const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+const idadeVer = minhaIdade > idadeAmiga
+const idadeDif = minhaIdade - idadeAmiga
+
+
+console.log("Sua idade é maior que a do seu amigo ?  " + idadeVer)
+
+console.log("Diferença da idade é  " + idadeDif)
 
 // EXERCICIO 02
 const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+// !b && d
+
+// (b && a) && !d
+
+// a || d
+
+// c && !d
