365168a0aa54f9d6b266f56b9f6568e691fc664a
devfelixarthur <de.felixarthur@gmail.com>
2022-07-20 21:06:07 -0300

    Aula008

--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -7,3 +7,13 @@ const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+
+// console.log ('Sua Idade é maior do que a do seu amigo?' , minhaIdade > idadeAmiga)
+// console.log ('A diferença de idade é', +(minhaIdade) - +(idadeAmiga))
+
+console.log (!b && d)
+console.log (b && a && !d)
+console.log (a || d)
+console.log (c && !d)
+
