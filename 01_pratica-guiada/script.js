2da8d71cf51051f63d5efd685f7c02efc3ff36f7
srheizenberg <cleitonfreelance@gmail.com>
2022-07-20 21:02:49 -0300

    aula 008 exercicios feito

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,63 @@
 //CODIGO AQUI
+/*let saldo = 15 + 22.40
+saldo = saldo - 12.34
+
+console.log("seu saldo total e de", saldo)
+
+
+saldo = saldo / 3 
+console.log("saldo total por irmão e de ", saldo)
+
+saldo = saldo *3,42
+console.log("total do investimento", saldo)
+
+saldo = saldo % 3 
+console.log("ainda tenho esse dinhero de saldo", saldo)
+
+const somaTotal = 2*2+2
+console.log(somaTotal)
+
+const somaTotal2 = 2*(2+2)
+console.log(somaTotal2) */
+/*
+let comparacao = 5 === 5
+console.log("comparacao", comparacao)
+
+let comparacao2 = 5 !== 5
+console.log("comparacao", comparacao2)
+
+let comparacao3 = 5 !== "5"
+console.log("comparacao", comparacao3)
+*/
+
+/*
+const num1 = 2
+const num2 = 8
+let resultado
+
+resultado = num1 == num2
+console.log(resultado)
+
+
+resultado = num1 <= num2
+console.log(resultado)
+
+
+resultado = num1 > num2
+console.log(resultado)
+
+resultado = num1 < num2
+console.log(resultado) */
+
+
+let a = "quero dirigi o celta 2012"
+let b = "não sou maior de idade"
+let c = "consigo compra um celta por 22.000"
+let d = "consigo alugar um celta"
+let e = "tenho carteira de motorista"
+
+a
+!b
+b && !e
+b && c || d
+b && e && !a
