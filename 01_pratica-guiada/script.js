22b1c6fc444503e70cb0ea1f19fabb5ae6606326
Tayanne <tayh.snovaes@gmail.com>
2022-07-20 21:12:38 -0300

    sobe os exercicio da aula

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,64 @@
 //CODIGO AQUI
+// let saldo = 15
+// let saldoAtualizado = saldo + 22.4
+// console.log(saldoAtualizado)
+
+// saldoAtualizado = saldoAtualizado - 12.34
+// console.log(saldoAtualizado)
+
+// let partePorIrmao = saldoAtualizado / 3
+// console.log(partePorIrmao)
+
+// let minhaParteAplicadaNaBolsa = partePorIrmao * 3.42
+// console.log(minhaParteAplicadaNaBolsa)
+
+// let dinheiroQueRESTOUProChiquetinho = minhaParteAplicadaNaBolsa % 3
+// console.log(dinheiroQueRESTOUProChiquetinho)
+
+// let operacao = 2 * 2 + 2
+// console.log(operacao)
+// // 2 * 2 = 4
+// // 4 + 2 = 6
+
+// let operacaoComPrecedencia = 2 * (2 + 2)
+// // 2 + 2 = 4
+// // 4 * 2 = 8
+// console.log(operacaoComPrecedencia)
+
+// const cincoEIgualACinco = 5 === 5
+// const cincoEhDiferenteDeCinco = 5 !== 5
+// const cincoNumberEhDiferenteDeCincoString = 5 !== '5'
+// const cincoStringEhIgualDeCincoCorrido = 5 === 'cinco'
+// const typeOfCincoEhIgualATypeOfVinte = typeof 5 === typeof 20
+// const typeOfCincoStringEhIgualTypeOfCincoCorrido = typeof '5' === typeof 'cinco'
+
+// console.log(cincoEIgualACinco)
+// console.log(cincoEhDiferenteDeCinco)
+// console.log(cincoNumberEhDiferenteDeCincoString)
+// console.log(cincoStringEhIgualDeCincoCorrido)
+// console.log(typeOfCincoEhIgualATypeOfVinte)
+// console.log(typeOfCincoStringEhIgualTypeOfCincoCorrido)
+
+// const numero1 = 15
+// const numero2 = 32
+
+// console.log('O primeiro numero é igual ao segundo', numero1 === numero2)
+// console.log('O primeiro numero é menor ou igual ao segundo', numero1 <= numero2)
+// console.log('O primeiro numero é maior ao segundo', numero1 > numero2)
+// console.log('O primeiro numero é menor que segundo', numero1 < numero2)
+
+// const a = 'Quero dirigir um Celta 2012'
+// const b = 'Sou maior de idade'
+// const c = 'Consigo comprar o Celta por 22.000 reais'
+// const d = 'Consigo alugar o Celta'
+// const e = 'Tenho carteira de motorista'
+
+// a
+// !b
+// b && (!e)
+// b && (c || d)
+// (b && e) && !a
+
+// binaria = 0 1
+// 0 = false | se nao tem nada na variavel, ela é 0, ela é false
+// 1 = true | se tiver alguma coisa na variavel, ela é 1, ela é true
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,9 +1,26 @@
 // EXERCICIO 01
-const minhaIdade = prompt('Qual sua idade?')
-const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+// const minhaIdade = prompt('Qual sua idade?')
+// const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+
+// console.log(
+//   'Sua idade é maior do que a do seu melhor amigo?',
+//   minhaIdade > idadeAmiga
+// )
+
+// console.log(
+//   'A diferença de idade é',
+//   (Number(minhaIdade) - Number(idadeAmiga)) * -1
+// )
 
 // EXERCICIO 02
 const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+// !b && d // E
+// (b && a) && !d
+// a || d // 0U
+// c && !d
+
+//Luana Torquato nao tem acesso a todos os videos do drive
