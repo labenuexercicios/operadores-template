0e8b26a5a71258590125351a4e9126000b3b3ae3
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2022-07-21 21:22:22 -0300

    exercicioDaAula

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,76 @@
 //CODIGO AQUI
+let saldo = 15
+let saldoAtualizado = saldo + 22.4
+console.log(saldoAtualizado);
+
+saldoAtualizado = saldoAtualizado - 12.34
+console.log(saldoAtualizado);
+
+let partePorIrmao = saldoAtualizado / 3
+console.log(partePorIrmao);
+
+let minhaParteAplicadaNaBolsa = partePorIrmao
+* 3.42
+console.log(minhaParteAplicadaNaBolsa)
+
+let dinheiroQueRestou = minhaParteAplicadaNaBolsa % 3
+console.log(dinheiroQueRestou)
+
+
+let operacao = 2 * 2 + 2
+console.log(operacao);
+// 2 * 2 = 4
+// 4 + 2 = 6
+
+let operacaoComPrecedencia = 2 * (2 + 2)
+// 2 + 2 = 4
+// 4 * 2 = 8
+console.log(operacaoComPrecedencia);
+
+
+
+
+//obs: falta corrigir o código!
+const cincoEhIgualACinco = 5 === 5
+const cincoEhDiferenteDeCinco = 5 !== 5
+const cincoNumberEhDiferenteDeCincoString = 5 !== '5'
+const cincoStringEhIgualDeCincoCorrido = 5 === 'cinco'
+const typeofCincoEhIgualATypeofVinte = typeof 5 === typeof 20
+const typeofCincoStringEhIgualTypeofCincoCorrido = typeof '5' === typeof 'cinco'
+
+console.log(cincoEhIgualACinco)
+console.log(cincoEhDiferenteDeCinco)
+console.log(cincoNumberEhDiferenteDeCincoString)
+console.log(cincoStringEhIgualDeCincoCorrido)
+console.log(typeofCincoEhIgualATypeofVinte)
+console.log(typeofCincoStringEhIgualTypeofCincoCorrido)
+
+
+
+const numero1 = 15
+const numero2 = 32
+
+console.log('O primeiro numero é igual ao segundo', numero1 === numero2);
+console.log('O primeiro numero é menor ou igual ao segundo', numero1 <= numero2)
+console.log('O primeiro numero é maior ao segundo', numero1 > numero2)
+console.log('O primeiro numero é menor que segundo', numero1 < numero2)
+
+
+/*const a = 'Quero dirigir um Celta 2012'
+const b = 'Sou maior de idade'
+const c = 'Consigo comprar o Celta por 22.000 reais'
+const d = 'Consigo alugar o Celta'
+const e = 'Tenho carteira de motorista'*/
+
+// a
+// !b
+// b && (!e)
+// b && (c || d)
+// (b && e) && !a
+
+
+
+
+
+
+
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,9 +1,20 @@
 // EXERCICIO 01
 const minhaIdade = prompt('Qual sua idade?')
 const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+console.log('Sua idade é maior do que a do seu amigo?', minhaIdade > idadeAmiga)
+console.log('A diferença de idade é', (Number(minhaIdade) - Number(idadeAmiga))) *-1
+
+
 
 // EXERCICIO 02
 const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+
+
+//!b && d
+//b && a, && !d
+//a || d
+//c && !d
