0a84ac414baaf05af04ede33b71af329284656d3
Gleice-Dourado <gleicielensmv@gmail.com>
2022-10-19 21:21:09 -0300

    Exercicios de prártica guiada e fixação

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,80 @@
 //CODIGO AQUI
+let saldo = 15 + 22.40
+
+console.log("Saldo", saldo)
+
+saldo = saldo - 12.34
+
+console.log("Saldo atualizado", saldo)
+ 
+saldo = saldo / 3
+
+console.log("Valor de cada irmão", saldo)
+
+saldo = saldo * 3.42
+
+console.log("Valor do investimento", saldo)
+
+saldo = saldo % 3
+
+console.log("Resto para o chiclete", saldo)
+
+// importancia do uso de parenteses
+
+const semparenteses = 2*2+2
+const comparenteses = 2*(2+2)
+
+console.log("----------------------------------------------------------")
+console.log("Importância do uso de parênteses:")
+console.log("2*2+2 =", semparenteses)
+console.log("2*(2+2) =", comparenteses)
+
+// replica - menor e igual < - >
+
+const n1 = 5
+const n2 = 12
+
+console.log("----------------------------------------------------------")
+console.log("operadores relacionais:")
+console.log("O primeiro numero é  igual ao segundo?", n1===n2)
+console.log("O primeiro numero é menor ou igual ao segundo?", n1<=n2)
+console.log("O primeiro numero é  maior que o segundo?", n1>n2)
+console.log("O primeiro numero é  menor que o segundo?", n1<n2)
+
+
+// Comparação
+
+const comparacaoIgual = 5 === 5
+console.log("5 === 5?", comparacaoIgual)
+
+const comparacaoDiferente = 5 !== 5
+console.log("5 !== 5?", comparacaoDiferente)
+
+const comparacaoNumberString = 5 !== 5
+console.log('5 !== "5"', comparacaoNumberString)
+
+const comparacaoStringString = "5"==="cinco"
+console.log('"5" === "cinco"', comparacaoStringString)
+
+const comparacaoTypeof1 =  typeof 5 === typeof 20
+console.log("typeof5 === typeof20:", comparacaoTypeof1)
+
+const comparacaoTypeof2 = typeof"5" === typeof "cinco"
+console.log('typeof"5" === typeof "cinco"', comparacaoTypeof2)
+// operadores logicos
+
+const a = "Quero dirigir um celta 2012"
+const b = "Sou maior de idade"
+const c = "Consigo comprar o celta por 22.000 reais"
+const d = "Consigo alugar o celta"
+const e = "Tenho carteira de motorista"
+
+// Quero dirigir o Celta 2012
+// Não sou maior de idade
+// Sou maior de idade e não tenho carteira de motorista
+// Sou maior de idade e consigo comprar ou alugar o celta
+
+/* !b
+b && !e
+b && (c||d)
+(b && e) && !a */
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -2,8 +2,29 @@
 const minhaIdade = prompt('Qual sua idade?')
 const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
 
+//outra maneira de fazer usando variavel para o calculo
+/* comparacaoIdades = minhaIdade - idadeAmiga
+diferencaIdade = minhaIdade - idadeAmiga */
+
+console.log("Sua idade é maior do que a do seu melhor amigo?", minhaIdade>idadeAmiga )
+//console.log("Sua idade é maior do que a do seu melhor amigo?", comparacaoIdades )
+
+console.log("A diferença de idade é de", minhaIdade - idadeAmiga, "anos")
+//console.log("A diferença de idade é de", diferencaIdade, "anos")
+
 // EXERCICIO 02
 const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+// a. não sou maior de idade e quero passear de carro
+// b. sou maior de idade e vou para a praia, e não quero passear de carro
+// c. Vou para a praia e quero passear de carro
+// d. Posso tomar uma cervejinha e não vou passear de carro
+
+/* !b && d
+(b && a) && !d
+a || d
+c && !d */
+
