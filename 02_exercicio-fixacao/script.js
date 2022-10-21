2a9a12a89164c59276d48e94b14623c97c7ec7b3
rafa07sr <rafa.07.g3@mail.com>
2022-10-20 21:15:02 -0300

    completei os exercicios

--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -2,8 +2,19 @@
 const minhaIdade = prompt('Qual sua idade?')
 const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
 
+console.log("sua idade é maior que a da sua ?", minhaIdade > idadeAmiga)
+console.log("a diferença é de" , minhaIdade - idadeAmiga, "anos" )
+
+
 // EXERCICIO 02
 const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+// resultado
+
+!b && d
+b && a && !d
+a || d
+c && !d
