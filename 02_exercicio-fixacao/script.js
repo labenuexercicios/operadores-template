5f7aa99332f87644a6798adbf86c278b05d9ec18
Pedro Henrique Rocha Machado <pdrohrocha.m@gmail.com>
2022-10-23 14:25:26 -0300

    commit do exercicio operadores template

--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,9 +1,24 @@
 // EXERCICIO 01
 const minhaIdade = prompt('Qual sua idade?')
 const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+let resposta1 = minhaIdade > idadeAmigo
+let resposta2 = minhaIdade - idadeAmigo
+
+console.log("Sua idade é maior que a do seu melhor amigo?", resposta1)
+console.log("A diferença entre sua idade e a do seu amigo é:", resposta2)
 
 // EXERCICIO 02
 const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+// a) Não sou maior de idade e quero passear de carro
+// b) Sou maior de idade e vou a praia e não quero passear de carro
+// c) vou para praia ou quero passear de carro 
+// d)Posso tomar uma cervejinha e não vou passear de carro
+
+!b && d         
+(b && a ) && !d 
+ a || d 
+ c && !d
