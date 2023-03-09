669bdcc71b9c046c3f5b5d9a9b2536aa7d132213
Índio Medeiros <71137294+indiomedeiros@users.noreply.github.com>
2023-03-09 15:19:55 -0300

    Update script.js

--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,9 +1,2 @@
-// EXERCICIO 01
-const minhaIdade = prompt('Qual sua idade?')
-const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+// EXERCICIO DE FIXAÇÃO
 
-// EXERCICIO 02
-const a = 'vou pra praia'
-const b = 'sou maior de idade'
-const c = 'posso tomar uma cervejinha'
-const d = 'quero passear de carro'
