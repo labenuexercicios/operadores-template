4ad79cb0589059f84baeca7c96f17730b702a40c
bandreid01 <viniciusdasilvax@gmail.com>
2023-03-15 14:52:48 -0300

    Resolução Exercício Prática Guiada

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,14 @@
 //CODIGO AQUI
+
+let saldo = 15
+console.log("SAldo Inicial:", saldo,"R$")
+let saldoAtual = saldo + 22.40
+console.log("Saldo Após Adicionar 22.40:", saldoAtual, "R$")
+saldoAtual -= 12.43
+console.log("Saldo Após Gastar 12.34: ", saldoAtual," R$")
+let divisaoIrmao = saldoAtual/2
+console.log("Após a divisão sobrou para mim", divisaoIrmao, "R$")
+let aplicacao = divisaoIrmao * 3.4
+console.log("Após a aplicação:" ,aplicacao, "R$")
+let divisaoIgual = aplicacao % 3
+console.log("Sobrou", divisaoIgual, "para comprar chicletes HAHAHA")
