e3e9dee7510bfc9304d6c8a707202feb66e4b04a
bandreid01 <viniciusdasilvax@gmail.com>
2023-03-15 15:12:37 -0300

    Resolução Exercícios de Fixação

--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,2 +1,9 @@
 // EXERCICIO DE FIXAÇÃO
 
+minhaIdade = Number(prompt("Qual a sua Idade: "))
+idadeAmigo = Number(prompt("Qual a Idade do seu melhor Amigo: "))
+
+console.log("Sua idade é maior que a do seu melhor amigo? ",minhaIdade > idadeAmigo)
+console.log("Diferença de Idade ", Math.abs(minhaIdade - idadeAmigo), "Anos")
+
+// comando math.abs torna o resultado sempre positivo para não ter idade negativa
