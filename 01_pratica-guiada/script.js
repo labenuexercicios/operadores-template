1a02b9283f79d23e6600eea9aad2c9921ad9d08e
rafaelmelodruzian <rmdconsultor@gmail.com>
2023-03-16 07:05:56 -0300

    exercicios-realizados

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,73 @@
-//CODIGO AQUI
+console.log("PRATICA GUIADA 1")
+// Pratica Guiada 1
+// 1
+const saldo = Number = ( ( 15.00) )
+console.log("Pratica 1 - 1) Saldo:", saldo, "reais.")
+// 2
+const aporte = Number = ( 22.40 )
+const saldo2 = Number = ( saldo + aporte)
+console.log("Pratica 1 - 2) Saldo:", saldo2, "reais.")
+// 3
+const gasto1 = Number = ( -12.34 )
+const saldo3 = saldo2 + gasto1
+console.log("Pratica 1 - 3) Saldo:", saldo3)
+//4
+const dividirIrmao = Number = ( 0.5 ) // 0.5 = metade ou 50%
+const saldo4 = saldo3 * dividirIrmao
+console.log("Pratica 1 - 4) Saldo:", saldo4)
+//5
+const aplicaçao = Number = ( 3.4 )
+const saldo5 = ( saldo4 * aplicaçao)
+console.log("Pratica 1 - 5) Saldo:", saldo5)
+//6 
+const partes = Number = ( ( 3 ) ) 
+const saldo6 = saldo5 / partes 
+console.log("Pratica 1 - 6) Saldo:", saldo6)
+//chiclete
+const chiclete = Number = ( ( 2.99) )
+const compraChicle = ( saldo6 >= chiclete) 
+console.log("Da para comprar um chiclete com o saldo restante:", compraChicle)
+
+console.log(" ")
+console.log("PRATICA GUIADA 2")
+// Pratica Guiada 2
+//1
+const pg21 = ( 5 === 5 )
+console.log(pg21)
+//2
+const pg22 = ( 5 !== 5 )
+console.log(pg22)
+//3
+const pg23 = ( 5 !== "5" )
+console.log(pg23)
+//4
+const pg24 = ( 5 === "cinco" )
+console.log(pg24)
+//5
+const pg25 = ( typeof 5 === typeof 20 )
+console.log(pg25)
+//6
+const pg26 = ( typeof"5" === typeof"cinco" ) 
+console.log(pg26)
+
+console.log(" ")
+console.log("PRATICA GUIADA 3")
+// Pratica Guiada 3
+const variavel1 = Number = (( 1389 ))
+const variavel2 = Number = (( 6723 ))
+
+const perg1 = ("O primeiro numero é igual ao segundo:")
+const perg2 = ("O primeiro numero é menor ou igual ao segundo:")
+const perg3 = ("O primeiro numero é maior que o segundo:")
+const perg4 = ("O primeiro numero é menor que o segundo:")
+
+const compara1 = variavel1 === variavel2
+const compara2 = variavel1 <= variavel2
+const compara3 = variavel1 > variavel2
+const compara4 = variavel1 < variavel2
+
+console.log(perg1, compara1)
+console.log(perg2, compara2)
+console.log(perg3, compara3)
+console.log(perg4, compara4)
+
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,2 +1,10 @@
-// EXERCICIO DE FIXAÇÃO
+let userAge = Number = ( prompt("Qual a sua idade?"))
+let friendAge = Number = ( prompt("Qual a idade do seu(ua) melhor amigo(a)?"))
+
+let comp1 = userAge > friendAge
+console.log(" Sua idade é maior que a do seu amigo.", comp1)
+
+
+let comp2 = userAge - friendAge
+console.log(" A diferença de idade entre vocês é:", comp2, "anos.")
 
