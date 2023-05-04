22888b7646fd69593be95394bff72d69dbe2604f
Barbara <barbaradasilvasantos23@gmail.com>
2023-05-04 20:59:14 -0300

    Praticas da aula completos

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,57 @@
-//CODIGO AQUI
+// CODIGO AQUI
+
+let saldo = 15
+ saldo = saldo + 22.40
+// console.log (saldo)
+
+saldo -= 12.34
+// console.log (saldo)
+
+saldo = saldo/2
+// console.log (saldo)
+
+saldo = saldo*3.4
+// console.log (saldo)
+
+saldo %=3
+// console.log (saldo)
+
+//-------------------------------------------------------
+// let operacao = 2 * 2 + 2 (ordem basica da matematica)
+let operacao = 2 * (2+2) // escolhendo a ordem da operacao
+// console.log (operacao)
+
+//-------------------------------------------------------
+const comparacao =  5 === 5
+//console.log (comparacao)
+
+const comparacao2 = 5 !== 5
+//console.log (comparacao2)
+
+const comparacao3 = 5 !== "5"
+// console.log (comparacao3)
+
+const comparacao4 = "5" === "cinco"
+// console.log (comparacao4)
+
+const comparacao5 = typeof 5 === typeof 20
+// console.log (comparacao5)
+
+const comparacao6 = typeof "5" === typeof "cinco"
+// console.log (comparacao6)
+
+//-------------------------------------------------------
+const primeiroNumero = 13
+const segundoNumero = 13
+
+const verificacao = primeiroNumero ===  segundoNumero
+console.log (verificacao)
+
+const verificacao2 = primeiroNumero <= segundoNumero
+console.log(verificacao2)
+
+const verificacao3 = primeiroNumero > segundoNumero
+console.log(verificacao3)
+
+const verificacao4 = primeiroNumero < segundoNumero
+console.log(verificacao4)
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,2 +1,14 @@
 // EXERCICIO DE FIXAÇÃO
+let idade = Number (prompt ("Qual a sua idade?"))
+console.log(idade);
+let idade2 = Number (prompt ("Qual a idade do seu amigo?"))
+console.log(idade2);
 
+const idadeAmigo = 23
+const minhaIdade = 22
+const comparacao = minhaIdade > idadeAmigo
+
+console.log("A sua idade é maior que a do seu amigo?",comparacao) 
+
+const diferenca = minhaIdade - idadeAmigo
+console.log(diferenca) 
