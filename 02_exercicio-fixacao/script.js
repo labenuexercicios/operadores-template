185fb3b32338930567f9b83cc3ea42d28e88fd31
Diego <diegogode.bonani@gmail.com>
2023-05-04 21:00:36 -0300

    exercicio

--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,2 +1,5 @@
-// EXERCICIO DE FIXAÇÃO
-
+const idadeSua = Number(prompt("Qual a sua idade?"))
+const idadeAmigo = Number(prompt("Qual a idade do seu melhor amigo?"))
+const comparacao = idadeSua > idadeAmigo
+console.log("A sua idade é maior que a do seu amigo?", comparacao)
+console.log(idadeSua - idadeAmigo)
