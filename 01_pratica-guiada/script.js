186f36ecc1e64ff2068517cdcf09d6d7fda66527
Rafael Simas <rafaelsimasdev@gmail.com>
2023-05-05 11:06:23 -0300

    tudo finalizado

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,80 @@
 //CODIGO AQUI
+// let saldo = 15
+
+// saldo += 22.40
+
+// saldo -= 12.34
+
+// saldo = saldo / 2
+
+// saldo = saldo * 3.4
+
+// saldo %= 3
+
+// console.log(saldo)
+
+// ---------------------------
+
+// let operacao = 2*2+2
+
+// let operacao = 2 * ( 2 + 2 )
+
+// console.log(operacao)
+
+// ------------------------------------
+
+// const comparacao = 5 === 5
+// const comparacao2 = 5 !== 5
+// const comparacao3 = 5 !== "5"
+// const comparacao4 = "5" === "cinco"
+// const comparacao5 = typeof 5 === typeof 20
+// const comparacao6 = typeof "5" === typeof "cinco"
+
+// console.log(comparacao)
+// console.log(comparacao2)
+// console.log(comparacao3)
+// console.log(comparacao4)
+// console.log(comparacao5)
+// console.log(comparacao6)
+
+// --------------------------------------
+
+// const num1 = 57
+// const num2 = 13
+
+// const verificacao = num1 === num2
+
+// console.log(verificacao)
+
+// const verificacao = num1 <= num2
+// const verificacao = num1 > num2
+// const verificacao = num1 < num2
+
+// console.log(verificacao)
+
+// ---------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ operadores/script.js
@@ -0,0 +1,74 @@
+// ========== PRATICA 1 ===============
+
+// let saldo = 15
+// // saldo = 15 + 22.40
+// saldo += 22.40
+// //37.4
+// saldo -= 12.34
+// //25.06
+// saldo /= 2
+// //12.53
+// saldo *= 3.40
+// //42.602
+// saldo %= 3
+// //0.6019999
+// console.log(saldo)
+
+// let conta = 2*2+2
+// //6
+// let conta2 = 2*(2+2)
+// //8
+
+// console.log(conta)
+// console.log(conta2)
+
+// ========== PRATICA 2 ===============
+
+// let conta = 5===5
+// //true
+// let conta2 = 5!==5
+// //false
+// let conta3 = 5!=="5"
+// //true
+// let conta4 = "5" === "cinco"
+// //false
+// let conta5 = typeof 5 === typeof 20
+// //true
+// let conta6 = typeof "5" === typeof "cinco"
+// //true
+
+// console.log(conta)
+// console.log(conta2)
+// console.log(conta3)
+// console.log(conta4)
+// console.log(conta5)
+// console.log(conta6)
+
+// ========== PRATICA 3 ===============
+
+// let a = 5
+// let b = 9
+
+// console.log(a === b)
+// //false
+
+// console.log(a <= b)
+// //true
+
+// console.log(a >b)
+// //false
+
+// console.log(a < b)
+// //true
+
+// ========== fixação ===============
+
+// let idade1 = Number(prompt("Qual é sua idade?"))
+// let idade2 = Number(prompt("Qual a idade do seu melhor amigo?"))
+
+// console.log("Qual é a sua idade?", idade1)
+// console.log("Qual é a idade do seu melhor amigo?",idade2)
+
+
+// console.log("Sua idade é maior que a do seu amigo?", idade1 < idade2)
+// console.log("A diferença da sua idade para a do seu melhor amigo é de", idade2 - idade1, "ano(os).")
