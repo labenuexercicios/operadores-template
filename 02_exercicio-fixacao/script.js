ad62893935c4e925df1ede8dc5c4ddee63f91585
j0rdanaaa <j0rdanaaa@gmail.com>
2023-06-22 23:10:59 -0300

    exercício de fixação de operadores

--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -2,8 +2,22 @@
 const minhaIdade = prompt('Qual sua idade?')
 const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
 
-// EXERCICIO 02
-const a = 'vou pra praia'
-const b = 'sou maior de idade'
-const c = 'posso tomar uma cervejinha'
-const d = 'quero passear de carro'
+
+console.log(`Minha idade: $`)
+cont comparacao = minhaIdade<idadeAmiga
+
+console.log(`A sua idade é menor que a idade da sua amiga${
+console.log    
+    
+    // EXERCICIO 02
+const a = 'vou pra praia'// true
+const b = 'sou maior de idade'// true
+const c = 'posso tomar uma cervejinha'// true
+const d = 'quero passear de carro'// true
+
+
+const operacao1 = !b && d // false
+const operacao2 = b && a && !d // true && false => false
+const operação3 = a || d // true
+const operacao4 = c && !d // false
+
