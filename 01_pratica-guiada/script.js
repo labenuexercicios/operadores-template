8fd52b4854f9b35d5c1c7107d9a59a029331bb7e
j0rdanaaa <j0rdanaaa@gmail.com>
2023-06-22 23:08:18 -0300

    prática guiada operadores

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,68 @@
-//CODIGO AQUI
+/*//Operação número 1
+saldo = 15
+saldo = saldo +22,40
+console.log(`Valor do saldo após a primeira operação ${saldo}`)
+
+// operação número 2
+saldo = saldo - 12,34
+console.log(`Valor do saldo após a segunda operação ${saldo}`)
+
+// Operação número 3
+saldo = saldo / 3
+console.log(`Saldo de cada irmão: ${saldo}`)
+
+// Operação número 4
+saldo = saldo * 3,42
+console.log(`Meu saldo após investimento e ${saldo}`)
+
+// Operação número 5
+// saldo assumira o valor do resto da divisão do dinheiro por 3
+saldo = saldo % 3
+console.log(`Agora eu tenho ${saldo}para comprar meus dadinhos`)
+
+// Precedência aritmética
+console.log(2 * 2 + 2)
+console.log(2 * (2+2))
+
+// Prática guiada 2
+
+const resultado1 = 5 === 5
+console.log(resultado1)
+
+const resultado2 = 5 !== 5
+console.log(`Comparação 2 ${resultado2}`)
+
+const resultado3 = 5 !== '5'
+console.log(`Comparação 3 ${resultado3}´)
+
+
+
+
+
+*/
+// Prática guiada 3
+
+const primeiroNumero = 15
+const segundoNumero = 24
+
+// Comparação 1
+
+const compara1 = primeiroNumero === segundoNumero
+console.log(`O primeiro número é igual ao segundo? ${compara1}`)
+
+// Comparação número 2
+
+const compara2 = primeiroNumero < segundoNumero
+
+console.log(`O primeiro número é maior ou igual o segundo número?${compara2}`)
+
+// Comparação 3
+
+const compara3 = primeiroNumero > segundoNumero
+console.log(`O primeiro número é maior que o segundo número? ${compara3}`)
+
+// Comparação 4
+
+const compara4 = primeiroNumero < segundoNumero
+console.log(`O promeiro número é menor que o segundo número? ${compara4}`)
+
