aae80de58a571f16a8e93ebc7eac880f6717f139
Roberto Jesus <roliberdade1433@gmail.com>
2023-11-09 18:01:27 -0300

    exercicios-operadores

--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,2 +1,9 @@
 // EXERCICIO DE FIXAÇÃO
 
+const idadeUsu = prompt("Qual sua Idade")
+const idedeAmigo = prompt("Qual a Idade do sue amigo")
+const diferecaIdade = idadeUsu - idedeAmigo
+console.log("Sua Idade é maior que a do seu melhor amigo?")
+
+console.log(idadeUsu >= idedeAmigo)
+console.log("Diferença de Idade é", diferecaIdade,"anos")
